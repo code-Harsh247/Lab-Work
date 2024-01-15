48e00f7e1fc3a9d30e3e811fce830f5c49911d40
code-Harsh247 <117767098+code-Harsh247@users.noreply.github.com>
2024-01-15 15:37:33 +0530

    Update social.h

--- social.h
+++ social.h
@@ -1,75 +1,83 @@
-#include <stdlib.h>
+    #include <stdlib.h>
 
-#define limit 1000
-#define typeIndividual 1
-#define typeBusiness 2
-#define typeGroup 3
-#define typeOrganisation 4
+    #define limit 1000
+    #define typeIndividual 1
+    #define typeBusiness 2
+    #define typeGroup 3
+    #define typeOrganisation 4
 
-struct individual_;
-struct groups_;
-struct business_;
-struct organisation_;
+    struct individual_;
+    struct groups_;
+    struct business_;
+    struct organisation_;
 
-//STRUCTURE DEFINATIONS
-typedef struct Date{
-    int day;
-    int month;
-    int year;
-}date;
+    //STRUCTURE DEFINATIONS
+    typedef struct Date{
+        int day;
+        int month;
+        int year;
+    }date;
 
-typedef struct basic_{
-    int id;
-    char *name;
-    date creationDate;
-    char *content[limit];
-}basic;
+    typedef struct basic_{
+        int id;
+        int contentCount;
+        char *name;
+        date creationDate;
+        char *content[limit];
+    }basic;
 
-typedef struct coordinate{
-    float x;
-    float y;
-}coords;
+    typedef struct coordinate{
+        float x;
+        float y;
+    }coords;
 
-typedef struct business_{
-    basic biz;
-    coords location;
-    int ownerCount;
-    int customerCount;
-    struct individual_ *customers[limit];
-    struct individual_ *owners[limit];
-    struct group_ *memberOf[limit];
-}business;
+    typedef struct business_{
+        basic biz;
+        coords location;
+        int ownerCount;
+        int customerCount;
+        int memberOfCount;
+        struct individual_ *customers[limit];
+        struct individual_ *owners[limit];
+        struct groups_ *memberOf[limit];
+    }business;
 
-typedef struct groups_{
-    basic grps;
-    int indMemberCount;
-    int bizMemberCount;
-    struct individual_ *members[limit];
-    struct business_ *bizMembers[limit];
+    typedef struct groups_{
+        basic grps;
+        int indMemberCount;
+        int bizMemberCount;
+        struct individual_ *members[limit];
+        struct business_ *bizMembers[limit];
 
-}group;
+    }group;
 
-typedef struct organisation_{
-    basic org;
-    coords location;
-    int memberCount;
-    struct individual_ *members[limit];
-    
-}organisation;
+    typedef struct organisation_{
+        basic org;
+        coords location;
+        int memberCount;
+        struct individual_ *members[limit];
+        
+    }organisation;
 
-typedef struct individual_{
-    basic ind;
-    date birthday;
-    group *linkedGrps[limit];
-    business *ownerOf[limit];
-    business *customerOf[limit];
-    organisation *linkedOrg[limit];
+    typedef struct individual_{
+        basic ind;
+        date birthday;
+        group *linkedGrps[limit];
+        business *ownerOf[limit];
+        business *customerOf[limit];
+        int ownerOfCount;
+        int customerOfCount;
+        int linkedGrpsCount;
+        int linkedOrgsCount;
+        organisation *linkedOrgs[limit];
 
-}individual;
+    }individual;
 
-//STRUCTURE ARRAYS
-individual *individual_list[limit];
-business *biz_list[limit];
-group *grp_list[limit];
-organisation *org_list[limit];
+    //STRUCTURE ARRAYS
+    individual *individual_list[limit];
+    business *biz_list[limit];
+    group *grp_list[limit];
+    organisation *org_list[limit];
 
+    void listProfiles();
+    void displaySeparatorWithType(int profileType);
