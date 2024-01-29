942e9798b40ae408b777f88a8f69725e6e52fe3f
code-Harsh247 <117767098+code-Harsh247@users.noreply.github.com>
2024-01-30 01:40:42 +0530

    Update assignment2.py

--- assignment2.py
+++ assignment2.py
@@ -65,11 +65,16 @@ class MyApp:
         self.home_frame = tk.Frame(root)
         self.registration_frame = tk.Frame(root)
         self.sign_in_frame = tk.Frame(root)
+        self.post_login_frame = tk.Frame(root)
+        
+        self.back_button = tk.Button(root, text="Back", command=self.go_back, font=btn_font, width=18, height=1)
+        self.back_button.pack(side="bottom", pady=10)
 
         self.history = [] #store history of frames
 
         self.profile_type = None
         self.gender = None
+        self.User = None
 
         self.create_home_page()
         self.create_registration_page()
@@ -161,7 +166,6 @@ class MyApp:
 
     def create_registration_page(self):
 
-
         user_id_label = tk.Label(self.registration_frame, text="User Id:", font=text_font)
         self.user_id_entry_register = tk.Entry(self.registration_frame)
         password_label = tk.Label(self.registration_frame, text="Password:", font=text_font)
@@ -211,7 +215,7 @@ class MyApp:
         next_button.grid(row=11, column=0, columnspan=3, rowspan=3, sticky="nsew", pady=(50,0))
         self.registration_frame.grid_rowconfigure(11, weight=1)
 
-
+    
     def gender_radio_selected(self):
         selectedOption = self.gender_radio.get()
         self.gender = selectedOption
@@ -225,6 +229,16 @@ class MyApp:
             self.profile_type = "UG-Student"
         if selectedOption == 3 :
             self.profile_type = "PG-Student"
+
+    def show_post_login_page(self):
+        # Hide all frames
+        self.home_frame.pack_forget()
+        self.back_button.pack_forget()
+        self.registration_frame.pack_forget()
+        self.sign_in_frame.pack_forget()
+
+        # Show the post-login frame
+        self.post_login_frame.pack()
 
     def show_sign_in_page(self):
 
@@ -357,12 +371,16 @@ class MyApp:
             teacher_profile = Teacher(user_profile.name, user_profile.user_ID, user_profile.password, user_profile.age, employee_id, department, user_profile.profile_status, self.gender )
             teachers_list.append(teacher_profile)
             print(teacher_profile.name, teacher_profile.age, teacher_profile.department, teacher_profile.employee_id, teacher_profile.gender, teacher_profile.user_ID, teacher_profile.password, teacher_profile.tries)
-            
+    
+    def logout(self):
+        # Hide the post-login frame and show the home frame
+        self.post_login_frame.pack_forget()
+        self.back_button.pack(side="bottom", pady=10)
+        self.show_home_page()
     
     def sign_in(self):
         password = self.password_entry_signin.get()
         user_ID = self.user_id_entry_signin.get()
-
 
 
         for teacher in teachers_list:
@@ -371,6 +389,9 @@ class MyApp:
                 if(teacher.password == password and teacher.profile_status == "Active"):
                     teacher.tries = 3
                     print("Sign in successfull. Profile type - teacher")
+                    self.User = teacher
+                    self.create_post_login_page()
+                    self.show_post_login_page()
                 elif(teacher.password != password and teacher.profile_status == "Active"):
                     print("User found but incorrect password entered")
                     teacher.tries -= 1
@@ -392,6 +413,9 @@ class MyApp:
                 if(student.password == password and student.profile_status == "Active"):
                     student.tries = 3
                     print("UG student found. Sign in successfull")
+                    self.User = student
+                    self.create_post_login_page()
+                    self.show_post_login_page()
                 elif(student.password != password and student.profile_status == "Active"):
                     print("User found but incorrect password entered")
                     student.tries -= 1
@@ -400,7 +424,7 @@ class MyApp:
                         self.deactivatedInfo()
                         print("That account has been deactivated.")
                     else:
-                        self.incorrectPassword(student)
+                        self.incorrectPassword()
                 elif(student.password == password and student.profile_status != "Active"):
                     self.deactivatedInfo()
                     print("Deactivated")
@@ -414,6 +438,9 @@ class MyApp:
                 if(student.password == password and student.profile_status == "Active"):
                     student.tries = 3
                     print("PG student found. Sign in successfull")
+                    self.User = student
+                    self.create_post_login_page()
+                    self.show_post_login_page()
                 elif(student.password != password and student.profile_status == "Active"):
                     print("User found but incorrect password entered")
                     student.tries -= 1
@@ -430,6 +457,70 @@ class MyApp:
             else:
                 self.profileNotFound()
                 print("No profile with that userID found.")
+
+    def create_post_login_page(self):
+        
+        
+        # Create widgets for the post-login page
+        welcome_label = tk.Label(self.post_login_frame, text="Welcome to the Academic Unit!", font=("Helvetica", 16))
+        logout_button = tk.Button(self.post_login_frame, text="Logout", command=self.logout, font=btn_font)
+        name_label = tk.Label(self.post_login_frame, text="Name :     ", font=("Helvetica", 10))
+        user_id_label = tk.Label(self.post_login_frame, text="User ID :", font=("Helvetica", 10))
+        age_id_label = tk.Label(self.post_login_frame, text="Age :", font=("Helvetica", 10))
+        gender_label = tk.Label(self.post_login_frame, text="Gender :", font=("Helvetica", 10))
+        profile_type_label = tk.Label(self.post_login_frame, text="Profile Type :", font=("Helvetica", 10))
+        name = tk.Label(self.post_login_frame, text=self.User.name, font=("Helvetica", 10))
+        user_id = tk.Label(self.post_login_frame, text=self.User.user_ID, font=("Helvetica", 10))
+        age = tk.Label(self.post_login_frame, text=self.User.age,  font=("Helvetica", 10))
+        gender = tk.Label(self.post_login_frame, text=self.User.gender, font=("Helvetica", 10))
+        name_label.grid(row=1, column=0, sticky="e", padx=(20, 0))
+        name.grid(row=1, column=1, sticky="w", padx=(0, 20))
+        user_id_label.grid(row=2, column=0, sticky="e", padx=(20, 0))
+        user_id.grid(row=2, column=1, sticky="w", padx=(0, 20))
+        age_id_label.grid(row=3, column=0, sticky="e", padx=(20, 0))
+        age.grid(row=3, column=1, sticky="w", padx=(0, 20))
+        gender_label.grid(row=4, column=0, sticky="e", padx=(20, 0))
+        gender.grid(row=4, column=1, sticky="w", padx=(0, 20))
+        
+        if isinstance(self.User,Teacher):
+            department_label = tk.Label(self.post_login_frame, text="Department:", font=("Helvetica", 10))
+            department = tk.Label(self.post_login_frame, text=self.User.department, font=("Helvetica", 10))
+            employee_id_label = tk.Label(self.post_login_frame, text="Employee ID:", font=("Helvetica", 10))
+            employee_id = tk.Label(self.post_login_frame, text=self.User.employee_id, font=("Helvetica", 10))
+            profile_type = tk.Label(self.post_login_frame, text="Teacher", font=("Helvetica", 10))
+            profile_type_label.grid(row=5, column=0, sticky="e", padx=(20, 0))
+            profile_type.grid(row=5, column=1, sticky="w", padx=(0, 20))
+            department_label.grid(row=6, column=0, sticky="e", padx=(20, 0))
+            department.grid(row=6, column=1, sticky="w", padx=(0, 20))
+            employee_id_label.grid(row=7, column=0, sticky="e", padx=(20, 0))
+            employee_id.grid(row=7, column=1, sticky="w", padx=(0, 20))
+        elif isinstance(self.User, UndergraduateStudent):
+            department_label = tk.Label(self.post_login_frame, text="Department:", font=("Helvetica", 10))
+            department = tk.Label(self.post_login_frame, text=self.User.department, font=("Helvetica", 10))
+            roll_label = tk.Label(self.post_login_frame, text="Roll No:", font=("Helvetica", 10))
+            roll = tk.Label(self.post_login_frame, text=self.User.student_id, font=("Helvetica", 10))
+            profile_type = tk.Label(self.post_login_frame, text="Undergraduate Student", font=("Helvetica", 10))
+            profile_type_label.grid(row=5, column=0, sticky="e", padx=(20, 0))
+            profile_type.grid(row=5, column=1, sticky="w", padx=(0, 20))
+            department_label.grid(row=6, column=0, sticky="e", padx=(20, 0))
+            department.grid(row=6, column=1, sticky="w", padx=(0, 20))
+            roll_label.grid(row=7, column=0, sticky="e", padx=(20, 0))
+            roll.grid(row=7, column=1, sticky="w", padx=(0, 20))
+        elif isinstance(self.User, PostgraduateStudent):
+            research_area_label = tk.Label(self.post_login_frame, text="Research Area:", font=("Helvetica", 10))
+            research_area = tk.Label(self.post_login_frame, text=self.User.research_area, font=("Helvetica", 10))
+            roll_label = tk.Label(self.post_login_frame, text="Roll No:", font=("Helvetica", 10))
+            roll = tk.Label(self.post_login_frame, text=self.User.student_id, font=("Helvetica", 10))
+            profile_type = tk.Label(self.post_login_frame, text="Postgraduate Student", font=("Helvetica", 10))
+            profile_type_label.grid(row=5, column=0, sticky="e", padx=(20, 0))
+            profile_type.grid(row=5, column=1, sticky="w", padx=(0, 20))
+            research_area_label.grid(row=6, column=0, sticky="e", padx=(20, 0))
+            research_area.grid(row=6, column=1, sticky="w", padx=(0, 20))
+            roll_label.grid(row=7, column=0, sticky="e", padx=(20, 0))
+            roll.grid(row=7, column=1, sticky="w", padx=(0, 20))
+        # Pack widgets
+        welcome_label.grid(row=0, column=0, columnspan=2, pady=(20, 10))
+        logout_button.grid(row=9, column=2, pady=(20, 10))
 
     def profileNotFound(self):
         messagebox.showerror("Profile Not found","No profile with the given user ID exist in the database")
@@ -455,7 +546,7 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = MyApp(root)
 
-    back_button = tk.Button(root, text="Back", command=app.go_back, font=btn_font, width = 18, height= 1)
-    back_button.pack(side="bottom", pady=10)
+    # back_button = tk.Button(root, text="Back", command=app.go_back, font=btn_font, width = 18, height= 1)
+    # back_button.pack(side="bottom", pady=10)
 
     root.mainloop()
